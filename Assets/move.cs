d0d4309ac499ea3007a4631752ccb6d2a0283c4b
bkyui040413 <bkyui040413@gmail.com>
2017-01-19 15:33:50 +0800

    fuck!!!!!!

---
+++ Assets/move.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector2.up * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector2.down * Time.deltaTime);
+        }
+	}
+
+    void OnMouseDown()
+    {
+        transform.position = new Vector2(0 , 0);
+    }
+}
